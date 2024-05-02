964d5561a34e81624eefb0d04f9e43b48732908e
Bruno Tizer <brunomateus160601@gmail.com>
2024-05-02 13:25:17 -0300

    Update styles and server setup

--- server.js
+++ server.js
@@ -6,21 +6,22 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Configuração para servir arquivos estáticos
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
-  
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
-  
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
+  console.log('A user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
